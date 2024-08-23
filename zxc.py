b776900fa3b87a77ece829495f5e5c56d37ab8fe
woxie <woxie@mail.ru>
2024-08-23 12:42:30 +0400

    ended integration with message service and many little changes

--- zxc.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-then = datetime(2012, 3, 5, 23, 8, 15)        # Random date in the past
-now  = datetime.now()                         # Now
-duration = now - then 
-                        # For build-in functions
-print(duration)
